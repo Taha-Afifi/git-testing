03ebfe873ceb1badd925e0c76607ad2bf4e1b575
Taha Afifi <systemphonesexploring@gmail.com>
2021-06-23 19:17:49 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,23 @@
+let a = 1_00;
+let b = 2_00.5;
+let c = 1e6;
+let d = 2.4;
+
+console.log(Math.round(Math.min(a, b, c, d)));
+
+// --------------------------------
+
+
+console.log(Math.pow(a, Math.trunc(d)));
+
+// ----------------------------------------
+
+
+console.log(Math.trunc(d));
+console.log(Math.round(d));
+console.log(Math.floor(d));
+console.log(parseInt(d));
+
+// ----------------------------
+
+
